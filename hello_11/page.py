1fe03b1f582a6b412057e557b1e3f477b1e5e178
myhat123 <605162579@qq.com>
2022-03-23 17:11:09 +0800

    增加分页示例

---
+++ hello_11/page.py
@@ -0,0 +1,23 @@
+results = [x for x in range(1050)]
+
+def page(data, size=100):
+    '''分页'''
+
+    length = len(data)
+    pages = length//size
+
+    rec = []
+    i = 0
+    while i < pages:
+        start = i * size
+        end = (i+1) * size
+        rec.append(data[start:end])
+        i = i + 1
+
+    if length/size > pages:
+        rec.append(data[end:])
+
+    return rec
+
+x = page(results)
+print(x)
